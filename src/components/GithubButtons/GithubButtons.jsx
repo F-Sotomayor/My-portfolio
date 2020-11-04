327ab82c163702b874622f5c5be2de313308543c
F-Sotomayor <facundosotomayor95@hotmail.com>
2020-11-03 22:49:35 -0300

    update

--- src/components/GithubButtons/GithubButtons.jsx
+++ src/components/GithubButtons/GithubButtons.jsx
@@ -5,7 +5,7 @@ const GithubButton = () => (
   <>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio/fork"
+      href="https://github.com/F-Sotomayor/My-Portfolio/fork"
       data-icon="octicon-repo-forked"
       data-size="large"
       data-show-count="true"
@@ -15,11 +15,10 @@ const GithubButton = () => (
     </GitHubButton>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio"
+      href="https://github.com/F-Sotomayor/My-Portfolio"
       data-icon="octicon-star"
       data-size="large"
       data-show-count="true"
-      aria-label="Star cobidev/gatsby-simplefolio on GitHub"
     >
       Star
     </GitHubButton>
--- src/html.js
+++ src/html.js
@@ -16,6 +16,16 @@ export default function HTML({
   return (
     <html {...htmlAttributes}>
       <head>
+        <meta
+          property="og:image"
+          content="facundosotomayor.surge.sh/static/c1029bfac3d49624572a5494ab9701db/a5370/profile.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Facundo Sotomayor" />
+        <meta property="og:title" content="Facundo Sotomayor - Frontend Developer" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="facundosotomayor.surge.sh" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
